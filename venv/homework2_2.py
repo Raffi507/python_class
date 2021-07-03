f20070fd8e8b6a468ad7b829263bc058a695e561
Raffi <raffi.levonyan.l@gmail.com>
2021-07-03 04:02:16 +0400

    new homework

--- venv/homework2_2.py
+++ venv/homework2_2.py
@@ -1,4 +1,5 @@
-class circle:
+#1
+class Circle:
     def __init__(self, r):
         self.radius = r
 
@@ -11,7 +12,36 @@ class circle:
 a = int(input("tell me the r of the circle and I will tell you its area and perimeter\n>>> "))
 
 
-circle_1 = circle(a)
+circle_1 = Circle(a)
 
 print(circle_1.area())
 print(circle_1.perimeter())
+
+
+#2
+class Triangle:
+    def __init__(self, h, a, b, c):
+        self.height = h
+        self.a_side = a
+        self.b_side = b
+        self.c_side = c
+
+    def area(self):
+        return self.height * self.b_side / 2
+
+    def perimeter(self):
+        return self.a_side + self.b_side + self.c_side
+
+e = int(input("Tell me the height of the triangle\n>>> "))
+t = int(input("Tell me the side of the triangle\n>>> "))
+w = int(input("Tell me the base of the triangle\n>>> "))
+q = int(input("Tell me the another side of the triangle\n>>> "))
+
+triangle_1 = Triangle(e, t, w, q)
+
+
+if w + t < q or w + q < t or q + t < w:
+    print("There is no such triangle.")
+else:
+    print(f"The area is {triangle_1.area()}")
+    print(f"The perimeter is {triangle_1.perimeter()}")
